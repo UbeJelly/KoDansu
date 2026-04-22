8f235a8a775a59d37d13bdaf2f1aeb1a8553c058
Jelly <dinpixels@proton.me>
2026-04-22 17:47:15 +0800

    Set smallest window size limit to 240x150, updated window drag to be consistent, used _notification() to detect mouse entering and exiting window, updated default window size to 310x220

--- Resources/Core/Scripts/Window.gd
+++ Resources/Core/Scripts/Window.gd
@@ -1,7 +1,8 @@
 class_name MainWindow extends Control
 
 
-var dragging = false
+var dragging: bool = false
+var click_xy: Vector2 = Vector2.ZERO
 
 
 func _input(event: InputEvent) -> void:
@@ -9,20 +10,24 @@ func _input(event: InputEvent) -> void:
 		if event.button_index == BUTTON_LEFT:
 			if event.pressed:
 				dragging = true
+				click_xy = get_global_mouse_position()
 			else:
 				dragging = false
 
 		if event.button_index == BUTTON_WHEEL_UP:
-			OS.window_size += Vector2(10.0, 10.0)
+			OS.window_size += Vector2(10, 10)
+			print(OS.get_window_size())
 		if event.button_index == BUTTON_WHEEL_DOWN:
-			OS.window_size -= Vector2(10.0, 10.0)
+			if OS.window_size >= Vector2(240, 150):
+				OS.window_size -= Vector2(10, 10)
 
 		if event.button_index == BUTTON_RIGHT:
 			if event.pressed:
 				$Options.visible = true
 
 	if event is InputEventMouseMotion and dragging == true:
-		OS.set_window_position(OS.get_window_position() + get_global_mouse_position() - OS.get_window_size() / 2)
+		var new_xy: Vector2 = OS.get_window_position() + (event.position - click_xy)
+		OS.set_window_position(new_xy)
 
 
 func _on_About_meta_clicked(meta) -> void:
@@ -50,3 +55,11 @@ func _on_Window_gui_input(event) -> void:
 		if event.button_index == BUTTON_LEFT:
 			if $Options.visible == true:
 				$Options.visible = false
+
+
+func _notification(what) -> void:
+	match what:
+		NOTIFICATION_WM_MOUSE_ENTER:
+			mouse_default_cursor_shape = Control.CURSOR_MOVE
+		NOTIFICATION_WM_MOUSE_EXIT:
+			pass
